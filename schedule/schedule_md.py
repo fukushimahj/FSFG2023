c18b0853e4356f3e2bc485c15023c472d25df17e
Hajime Fukushima <fukushimahj@13351198034.guest.cc.tsukuba.ac.jp>
2023-11-02 15:29:47 +0900

    On branch main Changes to be committed: deleted:    .program.md.swp deleted:    .registration.md.swp deleted:    schedule/.program2.html.swp deleted:    schedule/.schedule_v2.py.swo new file:   schedule/program_pdf.md new file:   schedule/schedule_md.py

---
+++ schedule/schedule_md.py
@@ -0,0 +1,171 @@
+
+# time of presentation
+invited_t=45
+normal_t =20
+start_h = 9
+start_t = 30
+lunchi_t = 90
+kyukei_t = 20
+
+
+
+d = []
+
+
+#d+=[[" 但木謙一 "," 北海学園大学 "," I "," アルマ望遠鏡による遠方銀河観測 "]]
+#d+=[[" 喜友名正樹 "," 京都大学 "," N "," cold accretionと輻射フィードバックによる超大質量星形成 "]]
+#d+=[[" 東翔 "," 甲南大学 "," N "," Amplification and saturation of turbulent magnetic field in collapsing primordial gas clouds "]]
+#d+=[[" 鈴木悠太 "," 愛媛大学 "," N "," Quasar Environment and Feedback at z~2 Probed with Lyα Emitters and Continuum Selected Galaxies "]]
+#d+=[[" 佐藤恭輔 "," Hosei univ./NAOJ "," N "," すばる望遠鏡/HSC で探るこぐま座矮小楕円体銀河の形成メカニズム "]]
+#d+=[["rest"]]
+#d+=[[" 伊藤 茉那 "," 東北大学 "," N "," z>100の極初期宇宙における初代星の形成 "]]
+#d+=[[" 大向一行 "," 東北大学 "," I "," 宇宙初期の星形成 "]]
+#d+=[[" 尾形絵梨花 "," 筑波大学 "," N "," 遠方宇宙における超大質量ブラックホールの起源：浮遊する種ブラックホールの進化 "]]
+#d+=[[" 矢島秀伸 "," 筑波大学 "," I "," 超遠方銀河の形成過程：最新のシミュレーションと観測のレビュー "]]
+#d+=[[" 播金優一 "," 東京大学 "," I "," JWSTによる遠方銀河観測（TBD） "]]
+#d+=[["lunchi"]]
+#d+=[[" 倉田昂季 "," 甲南大学大学院 "," N "," 恒星風を考慮した星間物質降着による金属汚染により初代星が金属欠乏星になりうるか "]]
+#d+=[[" 稲吉恒平 "," 北京大学 "," I "," The assembly of first massive black holes and prospects of JWST observations "]]
+#d+=[[" 登口暁 "," 信州大学 "," N "," これまでの BluDOGs と JWST による high-z BluDOGs 研究 "]]
+#d+=[[" 中根美七海 "," 東京大学 "," N "," JWSTによる遠方銀河のLyα輝線観測で探る宇宙再電離 "]]
+#d+=[[" 仲野友将 "," 筑波大学 "," N "," 銀河衝突過程における多重AGN発現機構の解明 "]]
+#d+=[["next_day"]]
+#d+=[[" 竹内努 "," 名古屋大学素粒子宇宙物理学専攻 "," N "," High-Dimensional Statistical Analysis of Interstellar and Intergalactic Matter "]]
+#d+=[[" 山田麟 "," 名古屋大学 "," N "," 低金属量銀河WLMのCI観測: 観測の意義と戦略 "]]
+#d+=[[" 大森清顕クリストファ "," Nagoya University "," N "," Mergers and AGNs in the HSC-SSP: Do Mergers Trigger AGN Activity? "]]
+#d+=[[" 津久井崇史 "," Australian National University "," N "," Disk-driven galaxy transformation at z=4: insights from spatially resolved ALMA data "]]
+#d+=[[" 松木場亮喜 "," 京都大学 "," N "," 低金属量原始惑星系円盤におけるダストリング形成 "]]
+#d+=[[" 福井康雄 "," Nagoya University "," N "," 低金属量中間速度水素雲(IVC)とその起源 "]]
+
+d += [[" 大向一行 "," 東北大学 "," I "," 宇宙初期の星形成 "]]
+d += [[" 伊藤茉那 "," 東北大学 "," N "," z>100の極初期宇宙における初代星の形成 "]]
+d += [[" 石山智明 "," 千葉大学 "," N "," 高解像度宇宙論的シミュレーションに基づいた準解析的初代星形成モデルの開発 "]]
+d+=[["rest"]]
+d += [[" 東翔 "," 甲南大学 "," N "," Amplification and saturation of turbulent magnetic field in collapsing primordial gas clouds "]]
+d += [[" 定成健児エリック "," 東北大学 "," N "," 初代星形成における円盤分裂に対する乱流磁場の影響 "]]
+d += [[" 喜友名正樹 "," 京都大学 "," N "," cold accretionと輻射フィードバックによる超大質量星形成 "]]
+d += [[" 木村和貴 "," 東北大学 "," N "," 超大質量星形成における原始星構造と星周円盤の共進化 "]]
+d+=[["lunchi"]]
+d += [[" 郡和範"," 北海道大学 "," I "," 高赤方偏移21cm線観測で迫る宇宙論の謎 -超巨大ブラックホールの起源・ニュートリノ質量・ダークマターの正体・小スケール密度ゆらぎ-"]]
+d += [[" 石垣美歩 "," 国立天文台 "," I "," 天の川銀河の古い恒星系で探る宇宙初期の化学進化 "]]
+d += [[" 倉田昂季 "," 甲南大学 "," N "," 恒星風を考慮した星間物質降着による金属汚染により初代星が金属欠乏星になりうるか "]]
+d+=[["rest"]]
+d += [[" 播田實 "," 総研大/国立天文台 "," N ","重力崩壊型超新星におけるSc,Ti,Vの合成 "]]
+d += [[" 川下大響 "," 東京大学 "," N "," 炭素「予熱」効果が対不安定型超新星に与える影響 "]]
+d += [[" 岡田 寛子 "," 兵庫県立大学 "," N "," 超金属欠乏星で探る weak r-process の起源 "]]
+d+=[["rest"]]
+d += [[" 岡本崇 "," 北海道大学 "," I "," 銀河形成シミュレーション "]]
+d += [[" 佐藤恭輔 "," 法政大学/国立天文台 "," N "," すばる望遠鏡/HSC で探るこぐま座矮小楕円体銀河の形成メカニズム "]]
+d+=[["next_day"]]
+
+
+d += [[" 播金優一 "," 東京大学 "," I "," JWSTによる遠方銀河観測 (TBD) "]]
+d += [[" 登口暁 "," 信州大学 "," N "," これまでの BluDOGs と JWST による high-z BluDOGs 研究 "]]
+d += [[" 中根美七海 "," 東京大学 "," N "," JWSTによる遠方銀河のLyα輝線観測で探る宇宙再電離 "]]
+d+=[["rest"]]
+d += [[" 但木謙一 "," 北海学園大学 "," I "," アルマ望遠鏡による遠方銀河観測 "]]
+d += [[" 津久井崇史 "," Australian National University "," N "," Disk-driven galaxy transformation at z=4: insights from spatially resolved ALMA data "]]
+d+=[["lunchi"]]
+d += [[" 稲吉恒平 "," 北京大学 "," I "," The assembly of first massive black holes and prospects of JWST observations "]]
+d += [[" 鈴木悠太 "," 愛媛大学 "," N "," Quasar Environment and Feedback at z~2 Probed with Lyα Emitters and Continuum Selected Galaxies "]]
+d += [[" 谷川衝 "," 東京大学 "," N "," Gaiaによる不活性コンパクト連星探査とその形成過程の理論研究 "]]
+d+=[["rest"]]
+d += [[" 尾形絵梨花 "," 筑波大学 "," N "," 遠方宇宙における超大質量ブラックホールの起源：浮遊する種ブラックホールの進化 "]]
+d += [[" 鈴口智也 "," 京都大学 "," N "," 輻射フィードバックを考慮した連星種BHの降着成長 "]]
+d += [[" 大森清顕クリストファ "," 名古屋大学 "," N "," Mergers and AGNs in the HSC-SSP: Do Mergers Trigger AGN Activity? "]]
+d+=[["rest"]]
+d += [[" 桐原崇亘 "," 筑波大学 "," N "," 銀河とブラックホールの共進化過程における種ブラックホール質量の影響 "]]
+d += [[" 仲野友将 "," 筑波大学 "," N "," 銀河衝突過程における多重AGN発現機構の解明 "]]
+d += [[" 豊内大輔 "," 大阪大学 "," N "," 連星質量輸送に伴う輻射駆動円盤風の生成 "]]
+d += [[" 竹内努 "," 名古屋大学 "," N "," High-Dimensional Statistical Analysis of Interstellar and Intergalactic Matter "]]
+d+=[["next_day"]]
+
+d += [[" 矢島秀伸 "," 筑波大学 "," I "," 超遠方銀河の形成過程：最新のシミュレーションと観測のレビュー "]]
+d += [[" 平野信吾 "," 神奈川大学 "," N "," 最遠方銀河観測から迫る始原的パワースペクトル "]]
+d+=[["rest"]]
+d += [[" 福井康雄 "," 名古屋大学 "," N "," 低金属量中間速度水素雲(IVC)とその起源 "]]
+d += [[" 柘植紀節 "," 東京大学 "," N "," 大マゼラン雲における銀河間潮汐相互作用による星間ガスの進化と星形成 "]]
+d += [[" 山田麟 "," 名古屋大学 "," N "," 低金属量銀河WLMのCI観測: 観測の意義と戦略 "]]
+d+=[["lunchi"]]
+d += [[" 杉村和幸 "," 北海道大学 "," N "," 初代銀河形成におけるFUV/EUVフィードバックの役割 "]]
+d += [[" 松木場亮喜 "," 京都大学 "," N "," 低金属量原始惑星系円盤におけるダストリング形成 "]]
+d += [[" 横山将汰 "," 東京大学 "," N "," 初代宇宙線が駆動するプラズマ不安定性による銀河間空間の加熱 "]]
+d+=[["rest"]]
+d += [[" 前田龍之介 "," 東北大学 "," N "," 星形成フィードバック効果を考慮した高速ガス衝突による星団形成 "]]
+d += [[" 福島肇 "," 筑波大学 "," N "," 星団形成における星風の影響 "]]
+d += [[" 辰己賢太 "," 甲南大学 "," N "," 分子雲と相互作用する超新星残骸の流体シミュレーション :体積占有率と衝撃波速度の関係 "]]
+d += [[" 梅村雅之 "," 筑波大学 "," N "," おわりに "]]
+
+
+
+
+
+lists = []
+days = ["11月20日(月)", "11月21日(火)", "11月22日(水)"]
+
+iday = 0
+lists += ["<span style=\"font-size: 150%; color: black;\">"+days[iday]+"</span><br>"]
+lists += ["\n"]
+lists += ["| 時刻||名前|所属|講演タイトル|\n|---|-|----|:-----:|----|"]
+
+def get_start_time(time):
+  time_h = int((time+start_t) / 60) + start_h
+  time_t = time+start_t - 60*(time_h-start_h)
+  return str(time_h).zfill(2)+str(time_t).zfill(2)
+
+get_start_time(100)
+
+time = 0
+ii   = 0
+for i in range(len(d)):
+
+  if(d[i][0] == "lunchi"):
+    tstr = get_start_time(time)
+    lists += ["| "+tstr+"||昼食||"]
+    time += lunchi_t
+    continue
+
+  if(d[i][0] == "rest"):
+    tstr = get_start_time(time)
+    lists += ["| "+tstr+"||休憩||"]
+    time += kyukei_t
+    continue
+
+  if(d[i][0] == "next_day"):
+    lists += ["\n"]
+    iday  += 1
+    lists += ["<span style=\"font-size: 150%; color: black;\">"+days[iday]+"</span><br><br>"]
+    lists += ["\n"]
+    lists += ["| 時刻||名前|所属|講演タイトル|\n|---|-|----|:-----:|----|"]
+    time = 0
+    continue
+
+  
+  if(d[i][2].replace(' ', '') == "I"):
+    invite = True
+  else:
+    invite = False
+
+  tstr = get_start_time(time)
+
+  if invite:
+    time += invited_t
+  else:
+    time += normal_t
+
+
+  if invite:
+    ii += 1
+    lists += ["|"+tstr+"|I|"+ d[i][0].replace(' ', '')+ " |" + d[i][1].replace(' ', '')+"| "+  d[i][3] +"|"]
+  else:
+    ii += 1
+    lists += ["|"+tstr+"|| "+ d[i][0].replace(' ', '')+ " |" + d[i][1].replace(' ', '')+"| "+  d[i][3] +"|"]
+
+filename = "program_pdf.md"
+f = open(filename, 'w')
+for lis in lists:
+    f.write(lis)
+    f.write("\n")
+f.close()
+
+print(ii)
